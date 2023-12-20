1616f437cfd4974d1dd94e6b74cde95e9f620861
ex100 <ex100@win2k18.local>
2023-12-20 09:38:53 +0530

    Search algorithm

--- src/main/java/com/abc/mavendemo/App.java
+++ src/main/java/com/abc/mavendemo/App.java
@@ -4,10 +4,32 @@ package com.abc.mavendemo;
  * Hello world!
  *
  */
+import java.lang.*;
+import java.util.*;
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	binarysearch bs = new binarysearch();
+    	methods obj = new methods();
+        Scanner sc= new Scanner(System.in);
+        System.out.println( "ENTER THE SIZE OF ARRAY:" );
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        obj.readelements(arr, n);
+        obj.printelements(arr);
+        obj.sort(arr);
+        obj.printelements(arr);
+        System.out.print("Enter serach element:");
+        int key = sc.nextInt();
+        int res=bs.binarySearch(arr, key, 0, arr.length-1);
+        if(res!=-404)
+        {
+        	System.out.print("element found at "+(res)+"th position");
+        }
+        else
+        {
+        	System.out.print("element not found");
+        }
     }
 }
