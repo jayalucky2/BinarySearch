6648996a1cd3532c5748149d7e2a9b34968fd6b2
Jayalakshmi <143499237+jayalucky2@users.noreply.github.com>
2023-12-20 10:13:02 +0530

    Sorting algorthim

---
+++ src/main/java/com/abc/mavendemo/sort.java
@@ -0,0 +1,20 @@
+package com.abc.mavendemo;
+public  class Sort
+{
+static void sort(int arr[])
+{
+int n = arr.length;
+for(int i = 0; i < n-1;i++)
+{
+for(int j= i+1 ; j <n;j++)
+{
+if (arr[j] < arr[i])
+{
+int temp = arr[j];
+arr[j] = arr[i];
+arr[i] = temp;
+}
+}
+}
+}
+}
